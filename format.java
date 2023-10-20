fbfc2659c23565e8f3f8e9614a3d63aebc0388fb
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-10-20 12:47:26 -0300

    Update format.java

--- format.java
+++ format.java
@@ -9,3 +9,17 @@ public class x{
         
     }
 }
+//The main() method is required and you will see it in every Java program
+//For do some coments, you need to use: Java Multi-line Comments\; Multi-line comments start with /* and ends with */.
+//Any text between /* and */ will be ignored by Java.
+
+/*public class x{
+    public static void main(String args[]){
+        int x = 5;
+        int y = 5;
+        System.out.println("Hello World");
+        String teste = String.format("The sum of %d and %d is %d",x,y,x+y);
+        System.out.println(teste);
+        
+    }
+}*/
