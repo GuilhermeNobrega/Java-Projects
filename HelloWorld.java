f9e396e1093e6eafb9da6528bb31cb8c11afb364
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-10-20 08:35:38 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,21 +4,18 @@ public class Main {
   }
 }
 
-/*Claro, esse é um código Java simples que imprime "Hello World" no console. Vou explicar linha por linha:
-1. `public class Main {`
-   - Isso define uma classe chamada "Main". Em Java, o nome do arquivo deve corresponder ao nome da classe pública, portanto, o arquivo que contém esse código deve ser chamado "Main.java". As classes são a base da programação orientada a objetos em Java.
+// Isso define uma classe chamada "Main".
+public class Main {
+  // Aqui, você está declarando um método chamado "main".
+  // O método "main" é o ponto de entrada para um programa Java.
+  public static void main(String[] args) {
+    // Esta linha imprime "Hello World" no console.
+    // "System.out" é um objeto que representa a saída padrão (normalmente o console).
+    System.out.println("Hello World"); // O método "println" imprime uma linha de texto.
+  } // Isso fecha o método "main".
+} // Isso fecha a classe "Main".
 
-2. `public static void main(String[] args) {`
-   - Aqui, você está declarando um método chamado "main". O método "main" é o ponto de entrada para um programa Java. Ele é onde a execução do programa começa. "public" significa que esse método pode ser acessado de qualquer lugar. "static" significa que o
-     método pertence à classe em vez de uma instância específica da classe. "void" indica que o método não retorna nenhum valor. "String[] args" são argumentos que podem ser passados para o programa quando ele é executado a partir da linha de comando.
-
-3. `System.out.println("Hello World");`
-   - Esta linha imprime "Hello World" no console. "System.out" é um objeto que representa a saída padrão (normalmente o console). "println" é um método que imprime uma linha de texto e, em seguida, move o cursor para a próxima linha. Neste caso, ele imprime a string "Hello World".
-
-4. `}`
-   - Isso fecha o método "main".
-
-5. `}`
-   - Isso fecha a classe "Main".
-
-Quando você executa esse programa, ele imprimirá "Hello World" no console, indicando que o programa foi executado com sucesso. Este é um exemplo extremamente simples em Java, frequentemente usado para ilustrar a estrutura básica de um programa Java e o uso do método "main"./*
+//public static void main(String[] args) {
+//Aqui, você está declarando um método chamado "main". O método "main" é o ponto de entrada para um programa Java. Ele é onde a execução do programa começa. 
+//"public" significa que esse método pode ser acessado de qualquer lugar. "static" significa que o método pertence à classe em vez de uma instância específica da classe. 
+//"void" indica que o método não retorna nenhum valor. "String[] args" são argumentos que podem ser passados para o programa quando ele é executado a partir da linha de comando.
