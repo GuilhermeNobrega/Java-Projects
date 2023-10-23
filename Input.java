fdbd6d5327cf0834cdc82a51081b25e4eb86020b
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-10-23 10:54:08 -0300

    Update Input.java

--- Input.java
+++ Input.java
@@ -28,3 +28,18 @@ public class Main{
         System.out.println(soma);
     }
 }
+
+
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter a value for x:");
+int value = Integer.parseInt(scanner.nextLine());
+System.out.println("Enter a value for y:");
+int value1 = Integer.parseInt(scanner.nextLine());
+int sum = value + value1;
+System.out.printf("The sum of %d and %d is %d\n", value, value1, sum);
+
+
+
+//--
+String numberStr = "42";
+int number = Integer.parseInt(numberStr);
