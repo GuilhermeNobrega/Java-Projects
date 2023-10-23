6df8196de66a821d0fe8792a5b3df52698f9fa19
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-10-23 11:02:06 -0300

    Update Input.java

--- Input.java
+++ Input.java
@@ -43,3 +43,36 @@ System.out.printf("The sum of %d and %d is %d\n", value, value1, sum);
 //--
 String numberStr = "42";
 int number = Integer.parseInt(numberStr);
+
+
+//
+
+    public static void main(String[] args){
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter a value for x:");
+        String value = x.nextLine();
+        Scanner y = new Scanner(System.in);
+        System.out.println("Enter a value for y:");
+        String value1 = x.nextLine();
+        System.out.println("Union of x and y is " + x+y);
+    }
+//
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a value for x:");
+        int value = scanner.nextInt();
+        
+        System.out.println("Enter a value for y:");
+        int value1 = scanner.nextInt();
+        
+        String result = "Union of x and y is " + value + value1;
+        
+        System.out.println(result);
+    }
+}
+
