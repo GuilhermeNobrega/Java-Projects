ad146dc8865c88ca32afbc1e956aaadfce75b745
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-01-11 18:41:05 -0300

    Update x.java

--- Scripts/x.java
+++ Scripts/x.java
@@ -2,9 +2,12 @@ public class x{
     public static void main(String args[]){
         int x = 5;
         int y = 5;
+        boolean afirmacao =  true;
         System.out.println("Hello World");
         String teste = String.format("The sum of %d and %d is %d",x,y,x+y);
+        String frase = "Java é bom? ";
         System.out.println(teste);
+        System.out.print(frase + afirmacao);
         
     }
 }
