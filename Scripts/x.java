3242f166909732eda7e1f8e643d9be0f2d3e0bf3
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-01-11 18:37:47 -0300

    Create x.java

---
+++ Scripts/x.java
@@ -0,0 +1,10 @@
+public class x{
+    public static void main(String args[]){
+        int x = 5;
+        int y = 5;
+        System.out.println("Hello World");
+        String teste = String.format("The sum of %d and %d is %d",x,y,x+y);
+        System.out.println(teste);
+        
+    }
+}
