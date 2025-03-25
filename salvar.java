24c150f3e4340f8883ef8f76f79ac9768db09a60
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-03-25 12:09:07 -0300

    Update salvar.java

--- salvar.java
+++ salvar.java
@@ -40,6 +40,39 @@ public class Main
 
 
 
+
+
+
+
+
+
+abstract class Galaxia {
+    String location = "Via Láctea";
+
+    // Método abstrato (não pode ter implementação na classe abstrata, apenas assinatura)
+    public abstract void mostrarLocalizacao();
+}
+
+// Criando uma classe concreta que estende a classe abstrata
+class Terra extends Galaxia {
+    @Override
+    public void mostrarLocalizacao() {
+        System.out.printf("Localização: %s\n", location);
+        System.out.printf("Planeta Terra está contida aqui.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Criando um objeto da classe concreta (não da abstrata!)
+        Terra planeta = new Terra();
+        planeta.mostrarLocalizacao();  // Chama o método abstrato implementado
+    }
+}
+
+
+
+
  class Pessoa{
       String nome = "paulo";// variavel de instancia; será sobrescrita pelo método abixo
       void setName(String nome){
