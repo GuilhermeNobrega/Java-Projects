6068f83ac747d4c6fa1c34fe29bc6de29b602949
Mclaren Senna mp4/6 <100214869+GuilhermeNobrega@users.noreply.github.com>
2025-08-07 09:15:18 -0300

    Create Alfabeto.java

---
+++ src/ExerciciosGerais/Alfabeto.java
@@ -0,0 +1,12 @@
+public class StringFive {
+    public static void main(String[] args) {
+        char [] letras = new char[27];
+        String series = " ";
+        for (int i = 0; i<26;i++ ){
+            char ch = (char)('a'+i);
+            //series = series+ch;
+            System.out.println(letras[i]=ch);
+        }
+        //System.out.println(series);
+    }
+}
